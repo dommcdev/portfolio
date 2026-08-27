9f38ee1a88d4410d071bd5486407caf7b044c8ef
Dominic McDevitt <dominicmcdev@gmail.com>
2026-08-27 14:39:33 -0500

    get everything scaffolded out

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = 'Dominic McDevitt';
 export const SITE_DESCRIPTION =
-  'Personal portfolio and blog of Dominic McDevitt. Thoughts on Linux, AI, web development, and anything else that catches my interest.';
+  "Dominic's personal portfolio and writings. Thoughts on Linux, AI, web development, and anything else that catches my interest.";
